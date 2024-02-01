f76bb8c28f5103c33e28b580f9ca08fae7e071e2
Sting <bobadillasting@gmail.com>
2024-02-01 19:04:33 -0300

    initial commit

---
+++ src/main/java/ar/mikellbobadilla/SeriesPlusApplication.java
@@ -0,0 +1,13 @@
+package ar.mikellbobadilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriesPlusApplication.class, args);
+	}
+
+}
